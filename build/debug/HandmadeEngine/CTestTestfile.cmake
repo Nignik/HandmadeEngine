5fc4988060d1bba121cee4bea11fdd82f81ae447
Nignik <maksymilian.olbrys@gmail.com>
2025-08-22 23:26:24 +0200

    feat: backbuffer allocation

--- build/debug/HandmadeEngine/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/dev/GameEngines/HandmadeEngine/HandmadeEngine
-# Build directory: C:/dev/GameEngines/HandmadeEngine/build/debug/HandmadeEngine
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
